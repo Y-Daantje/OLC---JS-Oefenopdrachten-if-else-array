e7bd702b4e60484876f2524676930b6cb5ac00b4
Y-Daantje <302867649@student.rocmondriaan.nl>
2023-12-11 10:28:55 +0100

    loc oefenen

---
+++ main.js
@@ -0,0 +1,32 @@
+//opdr1
+const nmr = ['4', '5', '8', '12', '-1', '99'];
+nmr.shift('0')
+console.log('your number are: ', nmr);
+
+//opdr2
+const nmrs = ['0.33', '4.8', '11.1', '34.5', '129.1', '-2.56', '-17.4'];
+console.log('your number are: ', nmrs);
+
+//OPDR3
+const colors = ['rood', 'groen', 'geel', 'blauw', 'roze'];
+console.log('your colors are: ', colors);
+
+//opdr4
+nmr[3] = '21'
+console.log('new numbers are: ',nmr);
+
+//opdr5
+nmrs[0] = '3.3'
+nmrs[6] = '-1.74'
+console.log('new numbers are: ',nmrs);
+
+//opdr6
+colors[3] = 'parse'
+console.log('new colors are: ',colors);
+
+//opdr7
+colors.push('zwart')
+
+//opdr8
+nmrs.unshift('0')
+console.log('new numbers are: ',nmrs);
